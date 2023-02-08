1b9dbf8ddec5084a0c2b9e94e9128b92ce1704c1
Jakaria230161 <jakariamahmud016@gmail.com>
2023-02-08 21:58:21 +0600

    split the code

--- Js-practice/test.js
+++ Js-practice/test.js
@@ -127,3 +127,86 @@ console.log(convertedFahrenheit);
 function seeTime() {
   document.getElementById('time').innerHTML = Date();
 }
+
+// property length
+let text = "abcdefghij klmnopqrstuv wxyz"
+let textLength = text.length;  // Length use for finding properties length. but if you give any spaces in the string it would be counted as empty string then length will be increase. mane eikhane 26 ta letter ache tai 26 ta dekhabe length use kore r jodi space dei taile oitao count korbe and increase hobe. ami taile 2 ta space dibo then 26 + 2 = 28 hobe.
+console.log(textLength);
+
+// Escape characters \(back slash) and line boro hoile onno line a read korte  hole \ slash dite hobe, onnothay error dibe bcz js arekta statement vabbe. 80 characters er beshi read kora kothin tai new line a jaoowar jonne \ use korte hoy. tobe best hocche + othoba break tag use kora. bcz back slash anek browser support kre na
+let escapeChar = "We are come from \"Dhaka\" to here \
+for higher study"
+console.log(escapeChar);
+
+// String can be object > using new keyword
+let x = "life line";
+let y = new String("life line");
+console.log(typeof x);
+console.log(typeof y);
+
+/* 
+1, == compare only values
+2, === compare values and types
+3, but objects are same then can not be compared using double equals or triple equals. objects can not be compared.
+*/
+
+// String methods //
+
+// String slice(start, end) methods
+let str = "Apple, Banana, Jack-fruits";
+console.log(str.slice(7, 13));
+
+let sliceFruits = "Apple, Orange, Lemon";
+let sliceFruit = sliceFruits.slice(-13); // if you want to slice using right side(minis sign) and also can slice from one side.
+console.log(sliceFruit);
+
+// substr methods this is also slice types 
+let subStr = "Lemon, Orange, Apple";
+console.log(subStr.substr(7, 6)); // substr mane 1st kop ta holo starting kop and 2nd kop ta holo porer koto character porjonto kop ta nibe, jemon 7(lemon porjonto kete nise r 6 (mane koper porer 6 character nibe jemon orange 6 character))
+
+
+//  replace methods > jeta replace korbo seta dite hobe r replace kore jeta dibo setao dite hobe, eta mone rakhte hobe je 1st er tai shudhu raplace hobe,aro mone rakhte hobe je eta case sensitive tai jei case same haowa lagbe , choto boro case hole hobe na. tobe hae regular expression diye /g diye case insensitive kora jabe.
+let replaceStr = "I love Bangladesh & Bangladesh";
+let newReplaceStr = replaceStr.replace("Bangladesh", "Saudi Arabia");
+console.log(newReplaceStr);
+
+// toUpperCase methods
+let upper = "i love my country";
+let toUpper = upper.toUpperCase();
+console.log(toUpper);
+
+// toLowerCase methods
+let lower = "I LOVE MY COUNTRY";
+let toLower = lower.toLowerCase();
+console.log(toLower);
+
+// concat methods
+let txt1 = "Jakaria";
+let txt2 = "Mahmud";
+let concatWithTxt1AndTxt2 = txt1.concat(" ",txt2); // empty string for space(" ") and important things is kar upor concat ta korchi.
+console.log(concatWithTxt1AndTxt2);
+
+/* 
+[NOTE:All string methods return new string.
+  mone rakhte hobe je,protita string kintu ager ta change kore na, just notun ekta string create kore.original string can not be changed just it being replaced or nothing, string is immutable. that's why it can not be changed.]
+*/
+
+// trim methods> use for unnecessary spaces removed
+let trm = "      love world!      ";  // space are being removed by trim
+console.log(trm.trim());
+
+// charAt methods > jodi ekta string katte chao tahole charAt use korte hobe r konta index postion ta katbe ta bole dite hobe.
+let singleChar = "Love you";
+console.log(singleChar.charAt(1));
+console.log(typeof singleChar.charAt(500));  // charAt jodi exact sring na pay taile hocche ekta empty string return korbe.
+console.log(singleChar[1]);
+console.log(typeof singleChar[500]); // [] eivabe jodi katte hoy r jodi exact string na pay taile undefiend values return korbe.undefined also a data type and value
+
+// charCodeAt methods > return just unicode 16 values.
+let charCode = "Love you";
+console.log(charCode.charCodeAt(2));
+
+// split methods > jodi string ke array a convert korte hoy taile split methods use korai hobe buddhimaner kaj.tai split(er moddhe bole dite hobe ki dite split > piece piece korbo) > basically split always ekta array return kore.
+let splitMethods = "Love you";
+console.log(splitMethods.split("")); // blank string dile , protitare character re split korbe.
+console.log(splitMethods.split(" ")); // split call na dile function type dekhabe r call dile object dekhabe. ekhon ekta array return korbe
